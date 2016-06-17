05001795800b6487545555a344f2fcd0887419f5
EatMyBytes <345links@gmail.com>
2016-06-17 19:28:17 +0000

    Add files via upload

---
+++ keyEvt/keyEvt.js
@@ -0,0 +1,694 @@
+//© 2016  DOMINIC MIGUEL STRONG ALL RIGHTS RESERVED
+//345links@gmail.com
+var TKecho = true,
+    key;
+
+function Key(evt) {
+    key = evt.keyCode || evt.which;
+    //console.log(key);
+    ////////lowercase letters/////////
+    if (key == 97) { //a
+        if (TKecho == true) {
+            console.log("a_" + key);
+        }
+        return "a"
+    }
+
+    if (key == 98) {
+        if (TKecho == true) {
+            console.log('b_' + key);
+        }
+        return "b"
+    }
+
+    if (key == 99) {
+        if (TKecho == true) {
+            console.log("c_" + key);
+        }
+        return "c";
+    }
+    if (key == 100) {
+
+        if (TKecho == true) {
+            console.log("d_" + key);
+        }
+        return "d";
+    }
+
+    if (key == 101) {
+        if (TKecho == true) {
+            console.log("e_" + key);
+        }
+        return "e";
+    }
+
+    if (key == 102) {
+        if (TKecho == true) {
+            console.log("f_" + key);
+        }
+        return "f";
+    }
+
+    if (key == 103) {
+        if (TKecho == true) {
+            console.log("g_" + key);
+        }
+        return "g";
+    }
+
+    if (key == 104) {
+        if (TKecho == true) {
+            console.log("h_" + key);
+        }
+        return "h";
+    }
+
+    if (key == 105) {
+        if (TKecho == true) {
+            console.log("i_" + key);
+        }
+        return "i";
+    }
+
+    if (key == 106) {
+        if (TKecho == true) {
+            console.log("j_" + key);
+        }
+        return "j";
+    }
+
+    if (key == 107) {
+        if (TKecho == true) {
+            console.log("k_" + key);
+        }
+        return "k";
+    }
+
+    if (key == 108) {
+        if (TKecho == true) {
+            console.log("l_" + key);
+        }
+        return "l";
+    }
+
+    if (key == 109) {
+        if (TKecho == true) {
+            console.log("m_" + key);
+        }
+        return "m";
+    }
+
+    if (key == 110) {
+        if (TKecho == true) {
+            console.log("n_" + key);
+        }
+        return "n";
+    }
+
+    if (key == 111) {
+        if (TKecho == true) {
+            console.log("o_" + key);
+        }
+        return "o";
+    }
+
+    if (key == 112) {
+        if (TKecho == true) {
+            console.log("p_" + key);
+        }
+        return "p";
+    }
+
+    if (key == 113) {
+        if (TKecho == true) {
+            console.log("q_" + key);
+        }
+        return "q";
+    }
+
+    if (key == 114) {
+        if (TKecho == true) {
+            console.log("r_" + key);
+        }
+        return "r";
+    }
+
+    if (key == 115) {
+        if (TKecho == true) {
+            console.log("s_" + key);
+        }
+        return "s";
+    }
+
+    if (key == 116) {
+        if (TKecho == true) {
+            console.log("t_" + key);
+        }
+        return "t";
+    }
+
+    if (key == 117) {
+        if (TKecho == true) {
+            console.log("u_" + key);
+        }
+        return "u";
+    }
+
+    if (key == 118) {
+        if (TKecho == true) {
+            console.log("v_" + key);
+        }
+        return "v";
+    }
+
+    if (key == 119) {
+        if (TKecho == true) {
+            console.log("w_" + key);
+        }
+        return "w";
+    }
+
+    if (key == 120) {
+        if (TKecho == true) {
+            console.log("x_" + key);
+        }
+        return "x";
+    }
+
+    if (key == 121) {
+        if (TKecho == true) {
+            console.log("y_" + key);
+        }
+        return "y";
+    }
+
+    if (key == 122) {
+        if (TKecho == true) {
+            console.log("z_" + key);
+        }
+        return "z";
+    }
+
+    //////////CAPITAL LETTERS/////////
+
+    if (key == 65) {
+        if (TKecho == true) {
+            console.log("shift_A_" + key);
+        }
+        return "A";
+    }
+
+    if (key == 66) {
+        if (TKecho == true) {
+            console.log("B_" + key);
+        }
+        return "B";
+    }
+
+    if (key == 67) {
+        if (TKecho == true) {
+            console.log("C_" + key);
+        }
+        return "C";
+    }
+
+    if (key == 68) {
+        if (TKecho == true) {
+            console.log("D_" + key);
+        }
+        return "D";
+    }
+
+    if (key == 69) {
+        if (TKecho == true) {
+            console.log("E_" + key);
+        }
+        return "E";
+    }
+
+    if (key == 70) {
+        if (TKecho == true) {
+            console.log("F_" + key);
+        }
+        return "F";
+    }
+
+    if (key == 71) {
+        if (TKecho == true) {
+            console.log("G_" + key);
+        }
+        return "G";
+    }
+
+    if (key == 72) {
+        if (TKecho == true) {
+            console.log("H_" + key);
+        }
+        return "H";
+    }
+
+    if (key == 73) {
+        if (TKecho == true) {
+            console.log("I_" + key);
+        }
+        return "I";
+    }
+
+    if (key == 74) {
+        if (TKecho == true) {
+            console.log("J_" + key);
+        }
+        return "J";
+    }
+
+    if (key == 75) {
+        if (TKecho == true) {
+            console.log("K_" + key);
+        }
+        return "K";
+    }
+
+    if (key == 76) {
+        if (TKecho == true) {
+            console.log("L_" + key);
+        }
+        return "L";
+    }
+
+    if (key == 77) {
+        if (TKecho == true) {
+            console.log("M_" + key);
+        }
+        return "M";
+    }
+
+    if (key == 78) {
+        if (TKecho == true) {
+            console.log("N_" + key);
+        }
+        return "N";
+    }
+
+    if (key == 79) {
+        if (TKecho == true) {
+            console.log("O_" + key);
+        }
+        return "O";
+    }
+
+    if (key == 80) {
+        if (TKecho == true) {
+            console.log("P_" + key);
+        }
+        return "P";
+    }
+
+    if (key == 81) {
+        if (TKecho == true) {
+            console.log("Q_" + key);
+        }
+        return "Q";
+    }
+
+    if (key == 82) {
+        if (TKecho == true) {
+            console.log("R_" + key);
+        }
+        return "R";
+    }
+
+    if (key == 83) {
+        if (TKecho == true) {
+            console.log("S_" + key);
+        }
+        return "S";
+    }
+
+    if (key == 84) {
+        if (TKecho == true) {
+            console.log("T_" + key);
+        }
+        return "T";
+    }
+
+    if (key == 85) {
+        if (TKecho == true) {
+            console.log("U_" + key);
+        }
+        return "U";
+    }
+
+    if (key == 86) {
+        if (TKecho == true) {
+            console.log("V_" + key);
+        }
+        return "V";
+    }
+
+    if (key == 87) {
+        if (TKecho == true) {
+            console.log("W_" + key);
+        }
+        return "W";
+    }
+
+    if (key == 88) {
+        if (TKecho == true) {
+            console.log("X_" + key);
+        }
+        return "X";
+    }
+
+    if (key == 89) {
+        if (TKecho == true) {
+            console.log("Y_" + key);
+        }
+        return "Y";
+    }
+
+    if (key == 90) {
+        if (TKecho == true) {
+            console.log("Z_" + key);
+        }
+        return "Z";
+    }
+    //////////NUMBERS/////////////////
+    if (key == 48) {
+        if (TKecho == true) {
+            console.log("0_" + key);
+        }
+        return "0";
+    }
+
+    if (key == 49) {
+        if (TKecho == true) {
+            console.log("1_" + key);
+        }
+        return "1";
+    }
+
+    if (key == 50) {
+        if (TKecho == true) {
+            console.log("2_" + key);
+        }
+        return "2";
+    }
+
+    if (key == 51) {
+        if (TKecho == true) {
+            console.log("3_" + key);
+        }
+        return "3";
+    }
+
+    if (key == 52) {
+        if (TKecho == true) {
+            console.log("4_" + key);
+        }
+        return "4";
+    }
+
+    if (key == 53) {
+        if (TKecho == true) {
+            console.log("5_" + key);
+        }
+        return "5";
+    }
+
+    if (key == 54) {
+        if (TKecho == true) {
+            console.log("6_" + key);
+        }
+        return "6";
+    }
+
+    if (key == 55) {
+        if (TKecho == true) {
+            console.log("7_" + key);
+        }
+        return "7";
+    }
+
+    if (key == 56) {
+        if (TKecho == true) {
+            console.log("8_" + key);
+        }
+        return "8";
+    }
+
+    if (key == 57) {
+        if (TKecho == true) {
+            console.log("9_" + key);
+        }
+        return "9";
+    }
+
+    //miscellaneous keys 
+
+    if (key == 126) {
+        if (TKecho == true) {
+            console.log("~_" + key);
+        }
+        return "~";
+    }
+
+    if (key == 96) {
+        if (TKecho == true) {
+            console.log("`_" + key);
+        }
+        return "`";
+    }
+
+    if (key == 33) {
+        if (TKecho == true) {
+            console.log("!_" + key);
+        }
+        return "!";
+    }
+
+    if (key == 64) {
+        if (TKecho == true) {
+            console.log("@_" + key);
+        }
+        return "@";
+    }
+
+    if (key == 35) {
+        if (TKecho == true) {
+            console.log("#_" + key);
+        }
+        return "#";
+    }
+
+    if (key == 36) {
+        if (TKecho == true) {
+            console.log("$_" + key);
+        }
+        return "$";
+    }
+
+    if (key == 37) {
+        $
+        if (TKecho == true) {
+            console.log("%_" + key);
+        }
+        return "%";
+    }
+
+    if (key == 94) {
+        if (TKecho == true) {
+            console.log("^_" + key);
+        }
+        return "^";
+    }
+
+    if (key == 38) {
+        if (TKecho == true) {
+            console.log("&_" + key);
+        }
+        return "&";
+    }
+
+    if (key == 126) {
+        if (TKecho == true) {
+            console.log("~_" + key);
+        }
+        return "~";
+    }
+
+    if (key == 42) {
+        if (TKecho == true) {
+            console.log("*_" + key);
+        }
+        return "*";
+    }
+
+    if (key == 40) {
+        if (TKecho == true) {
+            console.log("(_" + key);
+        }
+        return "(";
+    }
+
+    if (key == 41) {
+        if (TKecho == true) {
+            console.log(")_" + key);
+        }
+        return ")";
+    }
+
+    if (key == 95) {
+        if (TKecho == true) {
+            console.log("__" + key);
+        }
+        return "_";
+    }
+
+    if (key == 45) {
+        if (TKecho == true) {
+            console.log("-_" + key);
+        }
+        return "-";
+    }
+
+    if (key == 43) {
+        if (TKecho == true) {
+            console.log("+_" + key);
+        }
+        return "+";
+    }
+
+    if (key == 61) {
+        if (TKecho == true) {
+            console.log("=_" + key);
+        }
+        return "=";
+    }
+
+    if (key == 91) {
+        if (TKecho == true) {
+            console.log("[_" + key);
+        }
+        return "[";
+    }
+
+    if (key == 123) {
+        if (TKecho == true) {
+            console.log("{_" + key);
+        }
+        return "{";
+    }
+
+    if (key == 93) {
+        if (TKecho == true) {
+            console.log("]_" + key);
+        }
+        return "]";
+    }
+
+    if (key == 125) {
+        if (TKecho == true) {
+            console.log("}_" + key);
+        }
+        return "}";
+    }
+    if (key == 92) {
+        if (TKecho == true) {
+            console.log(("\_") + key);
+        }
+        return "-broken keyCode-line 598- backslash";
+    }
+
+    if (key == 124) {
+        if (TKecho == true) {
+            console.log("|_" + key);
+        }
+        return "|";
+    }
+
+    if (key == 59) {
+        if (TKecho == true) {
+            console.log(";_" + key);
+        }
+        return ";";
+    }
+
+    if (key == 58) {
+        if (TKecho == true) {
+            console.log(":_" + key);
+        }
+        return ":";
+    }
+
+    if (key == 39) {
+        if (TKecho == true) {
+            console.log("'_" + key);
+        }
+        return "'";
+    }
+
+    if (key == 34) {
+        if (TKecho == true) {
+            console.log('"_' + key);
+        }
+        return '"';
+    }
+
+    if (key == 13) {
+        if (TKecho == true) {
+            console.log("enter_" + key);
+        }
+        return "enter";
+    }
+
+    if (key == 44) {
+        if (TKecho == true) {
+            console.log(",_" + key);
+        }
+        return ",";
+    }
+
+    if (key == 60) {
+        if (TKecho == true) {
+            console.log("<_" + key);
+        }
+        return "<";
+    }
+
+    if (key == 46) {
+        if (TKecho == true) {
+            console.log("._" + key);
+        }
+        return ".";
+    }
+
+    if (key == 62) {
+        if (TKecho == true) {
+            console.log(">_" + key);
+        }
+        return ">";
+    }
+
+    if (key == 47) {
+        if (TKecho == true) {
+            console.log("/_" + key);
+        }
+        return "/";
+    }
+
+    if (key == 63) {
+        if (TKecho == true) {
+            console.log("?_" + key);
+        }
+        return "?";
+    }
+
+    if (key == 32) {
+        if (TKecho == true) {
+            console.log("space_" + key);
+        }
+        return "space";
+    }
+
+
+}
